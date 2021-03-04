511d1a86ccfd4f1ddf1ae02301857184b01b2049
Ross McKay <rmckay@webaware.com.au>
2021-03-04 14:56:21 +1100

    recommend min PHP 7.4 if running really old PHP

--- views/requires-php.php
+++ views/requires-php.php
@@ -12,5 +12,5 @@ if (!defined('ABSPATH')) {
 				'https://secure.php.net/supported-versions.php'
 			); ?>
 	</p>
-	<p><?php printf(esc_html__('Please upgrade your website hosting. At least PHP %s is recommended.', 'dps-pxpay-for-wp-ecommerce'), '7.2'); ?></p>
+	<p><?php printf(esc_html__('Please upgrade your website hosting. At least PHP %s is recommended.', 'dps-pxpay-for-wp-ecommerce'), '7.4'); ?></p>
 </div>
